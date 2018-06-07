f587cfb7b256ad8c02cea06121a35f37deb6f345
krystiankolupajewCTS <32029@corbytechnicalschool.org>
2018-06-07 09:57:26 +0100

    Added Call History

--- Phoneword/App.xaml.cs
+++ Phoneword/App.xaml.cs
@@ -1,17 +1,20 @@
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-namespace Phoneword
+namespace Phoneword //name of the Project or solution
 {
     public partial class App : Application
     {
+        public static IList<string> PhoneNumbers { get; set; }
+
         public App()
         {
             InitializeComponent();
-            MainPage = new MainPage();
+            PhoneNumbers = new List<string>();
+            MainPage = new NavigationPage(new MainPage());
         }
-
         protected override void OnStart()
         {
             // Handle when your app starts
